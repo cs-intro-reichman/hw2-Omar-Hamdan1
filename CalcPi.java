9d4b0876ec005e32c603ed593bc87c33b1bac6b4
Omar Hamdan <omar.hamdan@post.runi.ac.il>
2025-11-07 13:57:42 +0200

    Fouth

--- CalcPi.java
+++ CalcPi.java
@@ -1,10 +1,11 @@
 public class CalcPi {
     public static void main(String[] args) {
-        int terms = Integer.parseInt(args[0]);  // number of terms to use
+
+        int terms = Integer.parseInt(args[0]);
         double sum = 0.0;
 
         for (int i = 0; i < terms; i++) {
-            double denominator = 2.0 * i + 1.0;
+            double denominator = (2.0 * i) + 1.0;
 
             if (i % 2 == 0) {
                 sum += 1.0 / denominator;
@@ -16,6 +17,6 @@ public class CalcPi {
         double piApprox = 4 * sum;
 
         System.out.println("pi according to Java: " + Math.PI);
-        System.out.println("pi, approximated: " + piApprox);
+        System.out.printf("pi, approximated:%6.16f%n", piApprox); // <-- EXACT SPACING
     }
 }
